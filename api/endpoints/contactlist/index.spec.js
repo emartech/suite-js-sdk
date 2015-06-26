5b5f73c80a32db7752b2c4548e2dccb52033e6a8
Gergely Nemeth <mail@nemethgergely.com>
2015-06-26 15:53:22 +0200

    Convert contactlist to the new api

--- api/endpoints/contactlist/index.spec.js
+++ api/endpoints/contactlist/index.spec.js
@@ -18,18 +18,24 @@ describe('Suite Contact List', function() {
       }
     });
 
-    api.create(0, 'new fancy list', [1, 2]);
+    api.create({
+      key_id: 'id',
+      name: 'new fancy list',
+      external_ids: [1, 2]
+    });
   });
 
   it('list a contact list members', function() {
 
     var api = ContactListApi.create({
       get: function(customerId, url) {
-        expect(url).to.equal('/contactlist/2/contacts/?offset=0&limit=10');
+        expect(url).to.equal('/contactlist/2/contacts');
       }
     });
 
-    api.list(0, 2, 0, 10);
+    api.list({
+      contactListId: 2
+    });
   });
 
 });
