3c246265fcec40c217b79dfd21b93dffda56cec6
Gergely Nemeth <mail@nemethgergely.com>
2015-07-02 15:24:46 +0200

    Adding new interface for segment endpoint

--- api/endpoints/segment/index.spec.js
+++ api/endpoints/segment/index.spec.js
@@ -4,26 +4,39 @@ var expect = require('chai').expect;
 var apiTest = require('../../../test-helper');
 var SegmentAPI = require('./');
 
-describe.skip('Suite Segment', function() {
+describe('Suite Segment', function() {
 
-  var sdkMethods = {
-    listSegments: {
-      method: 'get',
-      expectedUrl: '/filter'
-    }
-  };
+  it('list segments', function() {
 
-  apiTest.testSDKMethodResponse(SegmentAPI, sdkMethods);
+    var api = SegmentAPI.create({
+      get: function(customerId, url, options) {
+        expect(url).to.equal('/filter');
+        expect(options.customerId).to.equal(1);
+      }
+    });
+
+    api.listSegments({
+
+    }, {
+      customerId: 1
+    });
+  });
 
   it('list contacts in a segment', function() {
 
     var api = SegmentAPI.create({
       get: function(customerId, url) {
-        expect(url).to.equal('/filter/10/contacts/limit=100&offset=0');
+        expect(url).to.equal('/filter/10/contacts/?limit=100&offset=0');
       }
     });
 
-    api.listContacts(0, 10, 0, 100);
+    api.listContacts({
+      segmentId: 10,
+      limit: 100,
+      offset:0
+    }, {
+      customerId: 1
+    });
   });
 
 });
