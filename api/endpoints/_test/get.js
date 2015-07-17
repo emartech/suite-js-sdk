c72939a16874ec313fae56521732c706b39b1e99
Gergely Nemeth <mail@nemethgergely.com>
2015-07-17 08:27:57 +0200

    Fixing unran test cases

--- api/endpoints/_test/get.js
+++ api/endpoints/_test/get.js
@@ -7,41 +7,45 @@ module.exports = {
     var apiEndpoint;
     var request;
 
+
     expectedData = expectedData || this._requestRespondWith;
 
     describe('towards ' + expectedUrl + ' endpoint', function() {
+
+      var self = this;
+
       beforeEach(function() {
-        request = this._getRequestStub();
-        apiEndpoint = this.ApiEndpoint.create(request, { customerId: 123 });
-      }.bind(this));
+        request = self._getRequestStub();
+        apiEndpoint = self.ApiEndpoint.create(request, { customerId: 123 });
+      });
 
       it('should properly call a GET request', function* () {
-        yield apiEndpoint[this.method](this.payload, this.options);
+        yield apiEndpoint[self.method](self.payload, self.options);
         expect(request.get).to.have.been.callCount(1);
         expect(request.get).to.have.been.calledWith(123, expectedUrl, {});
-      }.bind(this));
+      });
 
 
       it('should return the result', function* () {
-        var result = yield apiEndpoint[this.method](this.payload, this.options);
+        var result = yield apiEndpoint[self.method](self.payload, self.options);
         expect(request.get).to.have.been.callCount(1);
         expect(result).to.eql(expectedData);
-      }.bind(this));
+      });
 
 
       it('should pass the options to the suite request', function* () {
-        var options = _.merge({ customerId: 999, environment: 'otherEnv' }, this.options);
-        yield apiEndpoint[this.method](this.payload, options);
+        var options = _.merge({ customerId: 999, environment: 'otherEnv' }, self.options);
+        yield apiEndpoint[self.method](self.payload, options);
         expect(request.get).to.have.been.callCount(1);
         expect(request.get).to.have.been.calledWithExactly(sinon.match.any, sinon.match.any, options);
-      }.bind(this));
+      });
 
 
       it('should override customerId from the options', function* () {
-        yield apiEndpoint[this.method](this.payload, { customerId: 999 });
+        yield apiEndpoint[self.method](self.payload, { customerId: 999 });
         expect(request.get).to.have.been.callCount(1);
         expect(request.get).to.have.been.calledWithExactly(999, sinon.match.any, { customerId: 999 });
-      }.bind(this));
+      });
     }.bind(this));
   }
 };
--- api/endpoints/_test/put.js
+++ api/endpoints/_test/put.js
@@ -8,38 +8,41 @@ module.exports = {
     var request;
 
     describe('towards ' + expectedUrl + ' endpoint', function() {
+
+      var self = this;
+
       beforeEach(function() {
-        request = this._getRequestStub();
-        apiEndpoint = this.ApiEndpoint.create(request, { customerId: 123 });
-      }.bind(this));
+        request = self._getRequestStub();
+        apiEndpoint = self.ApiEndpoint.create(request, { customerId: 123 });
+      });
 
       it('should properly call a PUT request', function* () {
-        yield apiEndpoint[this.method](this.payload, this.options);
+        yield apiEndpoint[self.method](self.payload, self.options);
         expect(request.put).to.have.been.callCount(1);
         expect(request.put).to.have.been.calledWith(123, expectedUrl, expectedPayload, {});
-      }.bind(this));
+      });
 
 
       it('should return the result', function* () {
-        var result = yield apiEndpoint[this.method](this.payload, this.options);
+        var result = yield apiEndpoint[self.method](self.payload, self.options);
         expect(request.put).to.have.been.callCount(1);
-        expect(result).to.eql(this._requestRespondWith);
-      }.bind(this));
+        expect(result).to.eql(self._requestRespondWith);
+      });
 
 
       it('should pass the options to the suite request', function* () {
-        var options = _.merge({ customerId: 999, environment: 'otherEnv' }, this.options);
-        yield apiEndpoint[this.method](this.payload, options);
+        var options = _.merge({ customerId: 999, environment: 'otherEnv' }, self.options);
+        yield apiEndpoint[self.method](self.payload, options);
         expect(request.put).to.have.been.callCount(1);
         expect(request.put).to.have.been.calledWithExactly(sinon.match.any, sinon.match.any, sinon.match.any, options);
-      }.bind(this));
+      });
 
 
       it('should override customerId from the options', function* () {
-        yield apiEndpoint[this.method](this.payload, { customerId: 999 });
+        yield apiEndpoint[self.method](self.payload, { customerId: 999 });
         expect(request.put).to.have.been.callCount(1);
         expect(request.put).to.have.been.calledWithExactly(999, sinon.match.any, sinon.match.any, { customerId: 999 });
-      }.bind(this));
+      });
     }.bind(this));
   }
 };
