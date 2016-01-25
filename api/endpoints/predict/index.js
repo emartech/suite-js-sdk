78a284d67172a9393cae808e0e5ce2a1733349ce
gszabo <gabor.szabo@emarsys.com>
2016-01-25 17:07:11 +0100

    feat(predict-api): add hide/show widget operations

--- api/endpoints/predict/index.js
+++ api/endpoints/predict/index.js
@@ -17,6 +17,31 @@ _.extend(Predict.prototype, {
   listWidgets: function(payload, options) {
     logger.log('predict_list_widgets');
     return this._request.get(this._getCustomerId(options), '/predict', options);
+  },
+
+
+  hideWidget: function(payload, options) {
+    return this._requireParameters(payload, ['widget_id']).then(function() {
+      return this._sendToggleRequest(payload, options, 'hide');
+    }.bind(this));
+  },
+
+
+  showWidget: function(payload, options) {
+    return this._requireParameters(payload, ['widget_id']).then(function() {
+      return this._sendToggleRequest(payload, options, 'show');
+    }.bind(this));
+  },
+
+
+  _sendToggleRequest: function(payload, options, visibilityAction) {
+    logger.log('predict_toggle_widget');
+    return this._request.post(
+      this._getCustomerId(options),
+      util.format('/predict/%s/%s', payload.widget_id, visibilityAction),
+      this._cleanPayload(payload, ['widget_id']),
+      options
+    );
   }
 
 });
