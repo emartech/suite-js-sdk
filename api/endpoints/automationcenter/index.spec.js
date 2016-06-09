8a7a962a62942c8d05555d59a116257f0af8067d
tt1991 <tt1991518@hotmail.com>
2016-06-09 10:31:15 +0200

    feat(api): Add programsEntrypoints endpoint to automation center

--- api/endpoints/automationcenter/index.spec.js
+++ api/endpoints/automationcenter/index.spec.js
@@ -10,4 +10,24 @@ describe('SuiteAPI AutomationCenter endpoint', function() {
     testApiMethod(AutomationCenterAPI, 'programResource').withArgs({}).shouldThrowMissingParameterError('service_id');
     testApiMethod(AutomationCenterAPI, 'programResource').withArgs({ resource_id: 12 }).shouldThrowMissingParameterError('service_id');
   });
+
+  describe('#programsEntrypoints', function() {
+    testApiMethod(AutomationCenterAPI, 'programsEntrypoints')
+      .withArgs({ node_type: 12 })
+      .shouldThrowMissingParameterError('resource_id');
+
+    testApiMethod(AutomationCenterAPI, 'programsEntrypoints')
+      .withArgs({ resource_id: 12 })
+      .shouldThrowMissingParameterError('node_type');
+
+    testApiMethod(AutomationCenterAPI, 'programsEntrypoints')
+      .withArgs({})
+      .shouldThrowMissingParameterError('node_type', 'resource_id');
+
+    testApiMethod(AutomationCenterAPI, 'programsEntrypoints')
+      .withArgs({ node_type: 12, resource_id: 13, contact_id: 1 })
+      .shouldPostToEndpoint('/ac/programs/entrypoints/12/resources/13/runs', {
+        contact_id: 1
+      });
+  });
 });
